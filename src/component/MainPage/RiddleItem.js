4fe0112a50c845640f9addce7a467f649643f66c
Tastypotato245 <rbt2775@cau.ac.kr>
2024-05-16 02:18:46 +0900

    add: test code for state control

--- src/component/MainPage/RiddleItem.js
+++ src/component/MainPage/RiddleItem.js
@@ -5,21 +5,22 @@ function RiddleItem({ JWT, userInfo, setGameId, setNewGameId, riddle }) {
 
     const handleClick = async () => {
         try {
-            const response = await fetch('https://newturtles.newpotatoes.org/api/newgame', {
-                method: 'POST',
-                headers: {
-                    'Authorization' : `Bearer ${JWT}`,
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ jwt: JWT, userId: userInfo.email, riddleId: riddle.riddleId })
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok.');
-            }
-            const newGameId = await response.json();
-            setNewGameId(newGameId.newGameId);
-            setGameId(newGameId.newGameId);
+//            const response = await fetch('https://newturtles.newpotatoes.org/api/newgame', {
+//                method: 'POST',
+//                headers: {
+//                    'Authorization' : `Bearer ${JWT}`,
+//                    'Content-Type': 'application/json'
+//                },
+//                body: JSON.stringify({ jwt: JWT, userId: userInfo.email, riddleId: riddle.riddleId })
+//            });
+//
+//            if (!response.ok) {
+//                throw new Error('Network response was not ok.');
+//            }
+//            const newGameId = await response.json();
+//            setNewGameId(newGameId.newGameId);
+//            setGameId(newGameId.newGameId);
+            setGameId(riddle.riddleId);
         } catch (error) {
             console.error('Failed to fetch new game ID:', error);
         }
